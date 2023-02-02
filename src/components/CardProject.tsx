105f0389c9feb2a95562b670d4a40fd98c16d70e
Alberto Duran <albertoduranfilho@gmail.com>
2023-02-02 10:09:55 -0300

    Adiconado mais dois projetos ao portfólio

--- src/components/CardProject.tsx
+++ src/components/CardProject.tsx
@@ -8,7 +8,7 @@ export function CardProject(props: CardProjectProps){
     return(
         <div className="flex flex-col justify-center items-center mt-20">
             <h2 className=" mb-3 font-russo-one text-2xl text-secundary dark:text-backgroundlight" >{props.title}</h2>
-            <div className="w-96 h-60 rounded-lg border-b-4 border-primary duration-200  mx-4 hover:scale-95">
+            <div className="w-96 h-60 max-lg:w-80 rounded-lg border-b-4 border-primary duration-200  mx-4 hover:scale-95">
                 <a className="h-full w-full flex flex-col items-center justify-center " href={props.url} target='_blank' >
                     <img className=' h-full opacity-30 duration-150 hover:opacity-60' src={props.img} alt=""/>
                 </a>
--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -4,6 +4,9 @@ import { Heading } from "./Heading";
 import Montes from '../assets/montes-site.png'
 import vlc from '../assets/vlc-site.png'
 import GithubFinder from '../assets/github-finder.png'
+import NLWCopa from '../assets/nlw-copa.png'
+import RocketPay from '../assets/rocket-pay.png'
+import DesignSystem from '../assets/Ignite-lab-design-system.png'
 
 export function Projects(){
     return(
@@ -15,13 +18,17 @@ export function Projects(){
                 />
             </div>
 
-            <div className="flex justify-center ">
+            <div className="flex justify-center max-md:flex-col ">
                 <CardProject title="Montês Tecnologia" img={Montes} url='https://www.montestecnologia.com.br' />
                 <CardProject title="VLC Pedais" img={vlc} url='https://vlcpedais.com.br' />
             </div>
-            <div className="flex justify-center ">
+            <div className="flex justify-center max-md:flex-col ">
                 <CardProject title="GitHub Finder" img={GithubFinder} url='https://github.com/AlbertoDuranFilho/GitHub-Finder' />
-                <CardProject title="Montês Tecnologia" img={Montes} url='www.montestecnologia.com.br' />
+                <CardProject title="NLW Copa" img={NLWCopa} url='https://github.com/AlbertoDuranFilho/nlw-copa' />
+            </div>
+            <div className="flex justify-center max-md:flex-col ">
+                <CardProject title="RocketPay" img={RocketPay} url='https://github.com/AlbertoDuranFilho/explorer-lab-01' />
+                <CardProject title="Ignite Lab Design System" img={DesignSystem} url='https://github.com/AlbertoDuranFilho/ignite-lab-design-system-rocketseat' />
             </div>
         </div>
     )
